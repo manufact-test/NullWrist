260a01bb5638edfdcec05c95f7fb4a5248a75a1c
manufact-test <manufact.test@gmail.com>
2026-07-18 16:10:47 +0200

    Bump Android build for rear routing fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }

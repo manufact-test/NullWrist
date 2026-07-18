6bd3e35f60e511078382482ede6a183dd6a098ca
manufact-test <manufact.test@gmail.com>
2026-07-18 16:46:49 +0200

    Configure arm64 Android NDK build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,15 +5,26 @@ plugins {
 android {
     namespace = "com.manufacttest.pebblereardisplay"
     compileSdk = 36
+    ndkVersion = "28.0.13004108"
 
     defaultConfig {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
+
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += listOf("-std=c++17")
+            }
+        }
     }
 
     buildTypes {
@@ -23,6 +34,13 @@ android {
         }
         release {
             isMinifyEnabled = false
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
         }
     }
 

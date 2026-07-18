2d72a64855aeac2b7ebcc3bdcccb8ffd8db4e521
manufact-test <manufact.test@gmail.com>
2026-07-18 18:01:54 +0200

    Bump native QEMU probe build to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
 

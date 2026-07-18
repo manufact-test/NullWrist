bd4747e8871e16c5a9c8bee7a30c9117c0dc3d8c
manufact-test <manufact.test@gmail.com>
2026-07-18 22:48:52 +0200

    Bump version to 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.4.0"
+        versionCode = 9
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
 

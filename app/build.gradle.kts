2469536b1fccdc271e491b6d693cfa8fd6c421cc
manufact-test <manufact.test@gmail.com>
2026-07-19 01:56:19 +0200

    Bump version for persistent instant watchface switching

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.5.0"
+        versionCode = 12
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
 

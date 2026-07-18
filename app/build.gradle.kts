70ffffdb6eb94f3bf0aab190f3b7075b4745d463
manufact-test <manufact.test@gmail.com>
2026-07-18 22:19:13 +0200

    Bump version for PBW installation milestone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.3.1"
+        versionCode = 8
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
 

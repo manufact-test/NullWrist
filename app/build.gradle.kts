f8f946913f24b6e1ed436aec78cc93783db738fb
manufact-test <manufact.test@gmail.com>
2026-07-18 20:14:08 +0200

    Bump app for PebbleOS framebuffer probe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.2"
+        versionCode = 6
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
 

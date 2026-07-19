011ccebdbb9f7592c1f01dd17070de4542f8917c
manufact-test <manufact.test@gmail.com>
2026-07-19 18:42:55 +0200

    Bump Pebblehertz to 0.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.8.1"
+        versionCode = 16
+        versionName = "0.8.2"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
 

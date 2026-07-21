f0387b78d6591100a0f04e65315a1fe692bc753c
manufact-test <manufact.test@gmail.com>
2026-07-22 01:44:26 +0200

    Bump Stage 1 stability build code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 24
+        versionCode = 25
         versionName = "0.8.10"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"

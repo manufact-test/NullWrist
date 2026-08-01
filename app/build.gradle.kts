862f01bafc4490ed69ebc0b602f7539392737410
manufact-test <manufact.test@gmail.com>
2026-08-01 23:43:24 +0200

    Prepare NullWrist 0.8.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.manufacttest.pebblereardisplay"
         minSdk = 28
         targetSdk = 36
-        versionCode = 27
-        versionName = "0.8.10"
+        versionCode = 28
+        versionName = "0.8.11"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
 

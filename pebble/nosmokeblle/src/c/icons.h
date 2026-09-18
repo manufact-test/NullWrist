c6705b4392bd995514e5cb74cab438a5d625cd66
manufact-test <manufact.test@gmail.com>
2026-09-19 01:15:40 +0300

    Fix Pebble graphics context type

--- pebble/nosmokeblle/src/c/icons.h
+++ pebble/nosmokeblle/src/c/icons.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <pebble.h>
+#include <pebble_fonts.h>
 typedef enum { ICON_CIG, ICON_MONEY, ICON_CLOCK, ICON_LOCK, ICON_CHECK, ICON_BURST, ICON_UP, ICON_DOWN, ICON_SELECT } PixelIcon;
-void icon_draw(GraphicsContext *ctx,PixelIcon icon,GPoint p,int scale,GColor color);
+void icon_draw(GContext *ctx,PixelIcon icon,GPoint p,int scale,GColor color);
